d2ddd2dc015bdf58a2c684e7bdaa5a5d0bef324e
Jon-Erik <jdc151@case.edu>
2018-06-13 18:19:08 -0400

    Updates

--- src/components/Footer/Footer.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./Footer.css";
-
-const Footer = () => (
-	<p className="footer">React App © 2018 Chandler Productions</p>
-);
-
-export default Footer;
--- src/components/GameMessages/GameMessages.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import "./GameMessages.css";
-
-//Game messages component takes props from and is imported by Tiles component
-const GameMessages = props => (
-	<p className="messages"> Your Score: {props.currentScore} | Top Score: {props.topScore}<br/>{props.message}</p>
-);
-
-export default GameMessages;
